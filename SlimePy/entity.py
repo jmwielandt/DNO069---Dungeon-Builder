14600d0743cfe7accc9c0bf9ac582b58bcc192a1
Jose <jmwielandt@uc.cl>
2018-11-24 10:56:00 -0300

    port código movimiento colisiones

--- SlimePy/entity.py
+++ SlimePy/entity.py
@@ -1,21 +1,79 @@
 import parameters as p
 
+
 class HitBox:
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, parent_grid, mapx, mapy):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.speed_x = 0
         self.speed_y = 0
-        pass
+        self.pg = parent_grid
+        self.mapx = mapx
+        self.mapy = mapy
     
-    def move(self):
+    def coords(self):
+        return ((floor(self.x/p.TILE), floor(self.y/p.TILE)),
+            (floor((self.x + self.w - 0.01)/p.TILE), floor(self.y/p.TILE)),
+            (floor(self.x / p.TILE), floor((self.y + self.h - 0.01)/p.TILE)),
+            (floor((self.x + self.w - 0.01)/p.TILE), 
+             floor((self.y + self.h - 0.01)/p.TILE)))
+    
+    def move(self, sx, sy):
+        pos = self.coords()
+        if sx < 0:
+            if pos[0][0] == 0:
+                sx = max(sx, 0 - self.x)
+            else:
+                aux1 = self.pg[pos[0][0] - 1][pos[0][1]]
+                aux2 = self.pg[pos[2][0] - 1][pos[2][1]]
+                if aux1 == 1 or aux2 == 1:
+                    sx = max(sx, pos[0][0] * p.TILE - self.x)
+        else:
+            if pos[1][0] == self.mapx - 1:
+                sx = min(sx, self.mapx * p.TILE - self.x)
+            else:
+                aux1 = self.pg[pos[1][0] + 1][pos[1][1]]
+                aux2 = self.pg[pos[3][0] + 1][pos[3][1]]
+                if aux1 == 1 or aux2 == 1:
+                    sx = min(sx, (pos[1][0] + 1) * p.TILE - (self.x + self.w))
+        
+        self.speed_x = sx
         self.x += self.speed_x
+
+        if sy < 0:
+            if pos[0][1] == 0:
+                sy = max(sy, 0 - self.y)
+            else:
+                aux1 = self.pg[pos[0][0]][pos[0][1] - 1]
+                aux2 = self.pg[pos[1][0]][pos[1][1] - 1]
+                if aux1 == 1 or aux2 == 1:
+                    sy = max(sy, pos[0][1] * p.TILE - self.y)
+        else:
+            if pos[2][1] == self.mapy - 1:
+                sy = min(sy, self.mapy * p.TILE - self.y)
+            else:
+                aux1 = self.pg[pos[2][0]][pos[2][1] + 1]
+                aux2 = self.pg[pos[3][0]][pos[3][1] + 1]
+                if aux1 == 1 or aux2 == 1:
+                    sy = min(sy, (pos[2][1] + 1) * p.TILE - (self.y + self.h))
+        
+        self.speed_y = sy
         self.y += self.speed_y
+        
+        if self.x < 0:
+            self.x = 0
+        elif self.x + self.w >= width or self.x + self.w >= self.mapx * p.TILE:
+            self.x = min(width, self.mapx * p.TILE) - self.w
+        
+        if self.y < 0:
+            self.y = 0
+        elif self.y + self.h >= height or self.y + self.h >= self.mapy * p.TILE:
+            self.y = min(height, self.mapy * p.TILE) - self.h
+
 
 class Entity(HitBox):
-    def __init__(self, x, y, w, h):
-        HitBox.__init__(self, x, y, w, h)
+    def __init__(self, *args):
+        HitBox.__init__(self, *args)
         self.col = color(0, 255, 0)
-        pass
--- SlimePy/glob.py
+++ SlimePy/glob.py
@@ -14,7 +14,7 @@ class Juego:
             p.ESPACIO: False}  # espacio
         
         self.mapa = Mapa("data/nivel2.json")
-        self.jug = e.Entity(30, 50, 30, 30)
+        self.jug = e.Entity(30, 50, 30, 30, self.mapa.grilla, self.mapa.w, self.mapa.h)
         
         pass
     
@@ -22,29 +22,21 @@ class Juego:
         self.keys[keyCode] = st
     
     def update(self):
-        
         self.jug.speed_x = f.prox_zero(self.jug.speed_x, p.ROCE)
         self.jug.speed_y = f.prox_zero(self.jug.speed_y, p.ROCE)
         
         ## CONTINUAR
         if (self.keys[p.IZQUIERDA] and not self.keys[p.DERECHA]):
-            LX = self.jug.x // p.TILE
-            if LX == 0:
-                self.jug.speed_x = max(-p.SPEED, 0 - self.jug.x)
-            else:
-                self.jug.speed_x = -p.SPEED
+            self.jug.speed_x = -p.SPEED
         elif (not self.keys[p.IZQUIERDA] and self.keys[p.DERECHA]):
             self.jug.speed_x = p.SPEED
-        
+
         if (self.keys[p.ARRIBA] and not self.keys[p.ABAJO]):
             self.jug.speed_y = -p.SPEED
         elif (not self.keys[p.ARRIBA] and self.keys[p.ABAJO]):
             self.jug.speed_y = p.SPEED
-        
-        if self.jug.x < 0:
-            self.jug.x = 0
-        
-        self.jug.move()
+
+        self.jug.move(self.jug.speed_x, self.jug.speed_y)
         
         
 
@@ -53,6 +45,8 @@ class Mapa:
     def __init__(self, file):
         with open(file, "r") as data:
             self.grilla = json.load(data)
+        self.w = len(self.grilla)
+        self.h = len(self.grilla[0])
         
             
         pass
